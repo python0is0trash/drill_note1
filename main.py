604873c8b4bf3cbe57eba922439ef892421f1629
Python Is Trash <153543559+python0is0trash@users.noreply.github.com>
2024-02-14 22:23:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # from openpyxl.styles import (PatternFill)
+import datetime
 import sqlite3
 from flask import Flask, render_template, jsonify
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start_page():
-    connection = sqlite3.connect("./data_bases/students.sqlite3")
+    connection = sqlite3.connect("data_bases/students.sqlite3")
     cursor = connection.cursor()
     cursor.execute('''
         SELECT DISTINCT
@@ -28,12 +29,9 @@ def start_page():
             cadet
         FROM
             cadets
-        WHERE
-            platoon == {platoon}
         ORDER BY
             platoon, cadet;
-    '''
-                   .format(platoon=platoons[0][0]))
+    ''')
     cadets = cursor.fetchall()
     connection.commit()
     cursor.close()
@@ -42,10 +40,10 @@ def start_page():
         platoons[i] = platoons[i][0]
     for i in range(len(cadets)):
         cadets[i] = cadets[i][0]
-    return render_template('stroevka1.html', name=__name__,
+    return render_template('test.html', name=__name__,
                            platoons=platoons,
                            cadets=cadets,
-                           date_today="10.11.2023")
+                           date_today=str(datetime.date.today()))
 
 
 if __name__ == '__main__':
