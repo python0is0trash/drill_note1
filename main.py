f3d6b7259b9bedfa6584401bb6e1d58ae5493834
Python Is Trash <153543559+python0is0trash@users.noreply.github.com>
2024-02-16 14:43:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from openpyxl.styles import (PatternFill)
-import sqlite3
 from flask import Flask, render_template, jsonify
+from data_bases.db import select_info_from_db
+from datetime import datetime, date
 
 # workbook_fill = PatternFill(
 #     fill_type=None,
@@ -12,40 +13,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def start_page():
-    connection = sqlite3.connect("./data_bases/students.sqlite3")
-    cursor = connection.cursor()
-    cursor.execute('''
-        SELECT DISTINCT
-            platoon
-        FROM
-            cadets
-        ORDER BY
-            platoon;
-    ''')
-    platoons = cursor.fetchall()
-    cursor.execute('''
-        SELECT
-            cadet
-        FROM
-            cadets
-        WHERE
-            platoon == {platoon}
-        ORDER BY
-            platoon, cadet;
-    '''
-                   .format(platoon=platoons[0][0]))
-    cadets = cursor.fetchall()
-    connection.commit()
-    cursor.close()
-    connection.close()
-    for i in range(len(platoons)):
-        platoons[i] = platoons[i][0]
-    for i in range(len(cadets)):
-        cadets[i] = cadets[i][0]
     return render_template('stroevka1.html', name=__name__,
-                           platoons=platoons,
-                           cadets=cadets,
-                           date_today="10.11.2023")
+                           platoons=select_info_from_db()[0],
+                           cadets=select_info_from_db()[1],
+                           date_today="{}-{}-{}".format(date.today().day, date.today().month, date.today().year))
 
 
 if __name__ == '__main__':
